5527dcfbfe9d00564d541a8b7471b9f59296e5c0
fkslr <141187903+fkslr@users.noreply.github.com>
2024-01-21 19:12:55 +0500

    Add files via upload

---
+++ 5039203-1/Form1.cs
@@ -0,0 +1,34 @@
+namespace _5039203_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text.ToString() + textBox2.Text.ToString());
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
